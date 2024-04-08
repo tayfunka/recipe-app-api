5c0e187c8764f420e06e345fd17e92734b336a50
tayfun karakavuz <tayfun.karakavuz@gmail.com>
2024-04-08 04:26:48 +0300

    Added GitHub Actions

--- app/app/tests.py
+++
@@ -1,8 +0,0 @@
-from app import calc
-from django.test import SimpleTestCase
-
-
-class CalcTests(SimpleTestCase):
-    def test_add_number(self):
-        res = calc.add(5,6)
-        self.assertEquals(res, 11)   
